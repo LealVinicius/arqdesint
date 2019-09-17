1ef5c5709c4cc37e39a591f0102ce1b406a62d02
Vinicius <vinicius-leal1999@outlook.com>
2019-09-17 01:24:52 -0300

    Interceptor

--- src/main/java/br/usjt/hellospringboot/AppConfig.java
+++ src/main/java/br/usjt/hellospringboot/AppConfig.java
@@ -1,13 +1,25 @@
 package br.usjt.hellospringboot;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import br.usjt.hellospringboot.model.Calculadora;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import br.usjt.hellospringboot.controller.LoginInterceptor;
 import br.usjt.hellospringboot.model.Calculadora;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 	@Bean
-	public  Calculadora getCalcudora() {
+	public Calculadora getCalcudora() {
 		return new Calculadora();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login", "/",
+				"/fazerLogin");
 	}
 }
